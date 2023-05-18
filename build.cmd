fa14e794ec746d178cf40f9b26e1065ddd799e5a
Damian Hickey <dhickey@gmail.com>
2023-05-18 22:41:21 +0200

    Use newer docker image to build .net 6

--- build.cmd
+++ build.cmd
@@ -4,7 +4,7 @@ docker run --rm -it --name littleforker-build ^
   -v %cd%:/repo ^
   -w /repo ^
   -e FEEDZ_LITTLEFORKER_API_KEY=%FEEDZ_LITTLEFORKER_API_KEY% ^
-  damianh/dotnet-sdks:1 ^
+  damianh/dotnet-sdks:7 ^
   dotnet run -p build/build.csproj -c Release -- %*
 
 if errorlevel 1 (
--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ docker run --rm --name littleforker-build \
  -v $PWD:/repo \
  -w /repo \
  -e FEEDZ_LITTLEFORKER_API_KEY=$FEEDZ_LITTLEFORKER_API_KEY \
- damianh/dotnet-sdks:1 \
+ damianh/dotnet-sdks:7 \
  dotnet run -p build/build.csproj -c Release -- "$@"

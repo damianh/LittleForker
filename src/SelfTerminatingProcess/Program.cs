05049d76e09efb1141c09fef4a35030162ffc54d
Damian Hickey <dhickey@gmail.com>
2023-05-21 11:04:46 +0200

    top level programs

--- src/SelfTerminatingProcess/Program.cs
+++ src/SelfTerminatingProcess/Program.cs
@@ -1,11 +1,3 @@
 ﻿using System;
 
-namespace SelfTerminatingProcess;
-
-public class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Done.");
-    }
-}
+Console.WriteLine("Done.");

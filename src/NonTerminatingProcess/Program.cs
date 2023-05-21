76146c4197d1afa6ba1d7b4f235d20d3cd4a75d6
Damian Hickey <dhickey@gmail.com>
2023-05-21 11:04:46 +0200

    top level programs

--- src/NonTerminatingProcess/Program.cs
+++ src/NonTerminatingProcess/Program.cs
@@ -7,105 +7,79 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging.Abstractions;
 using Serilog;
 
-namespace NonTerminatingProcess
+var logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+var shutdown = new CancellationTokenSource(TimeSpan.FromSeconds(100));
+var configRoot = new ConfigurationBuilder()
+    .AddCommandLine(args)
+    .AddEnvironmentVariables()
+    .Build();
+
+// Running program with --debug=true will attach a debugger.
+// Used to assist with debugging LittleForker.
+if (configRoot.GetValue("debug", false))
 {
-    internal sealed class Program
+    Debugger.Launch();
+}
+
+var ignoreShutdownSignal = configRoot.GetValue<bool>("ignore-shutdown-signal", false);
+if (ignoreShutdownSignal)
+{
+    Log.Logger.Information("Will ignore Shutdown Signal");
+}
+
+var exitWithNonZero = configRoot.GetValue<bool>("exit-with-non-zero", false);
+if (exitWithNonZero)
+{
+    Log.Logger.Information("Will exit with non-zero exit code");
+}
+
+var pid = Process.GetCurrentProcess().Id;
+Log.Logger.Information($"Long running process started. PID={pid}");
+
+var parentPid = configRoot.GetValue<int?>("ParentProcessId");
+
+using (parentPid.HasValue
+    ? new ProcessExitedHelper(parentPid.Value, _ => ParentExited(parentPid.Value), new NullLoggerFactory())
+    : NoopDisposable.Instance)
+{
+    using (await CooperativeShutdown.Listen(ExitRequested, new NullLoggerFactory()))
     {
-        // Yeah this process is supposed to be "non-terminating"
-        // but we don't want tons of orphaned instances running
-        // because of tests so it terminates after a long
-        // enough time (100 seconds)
-        private readonly CancellationTokenSource _shutdown = new CancellationTokenSource(TimeSpan.FromSeconds(100));
-        private readonly IConfigurationRoot _configRoot;
-        private readonly bool _ignoreShutdownSignal;
-        private readonly bool _exitWithNonZero;
-
-        static Program()
+        // Poll the shutdown token in a tight loop
+        while (!shutdown.IsCancellationRequested || ignoreShutdownSignal)
         {
-            Log.Logger = new LoggerConfiguration()
-                .WriteTo.Console()
-                .CreateLogger();
+            await Task.Delay(100);
         }
-
-        private Program(string[] args)
-        {
-            _configRoot = new ConfigurationBuilder()
-                .AddCommandLine(args)
-                .AddEnvironmentVariables()
-                .Build();
-
-            // Running program with --debug=true will attach a debugger.
-            // Used to assist with debugging LittleForker.
-            if (_configRoot.GetValue("debug", false)) 
-            {
-                Debugger.Launch();
-            }
-            
-            _ignoreShutdownSignal = _configRoot.GetValue<bool>("ignore-shutdown-signal", false);
-            if (_ignoreShutdownSignal)
-            {
-                Log.Logger.Information("Will ignore Shutdown Signal");
-            }
-
-            _exitWithNonZero = _configRoot.GetValue<bool>("exit-with-non-zero", false);
-            if (_exitWithNonZero)
-            {
-                Log.Logger.Information("Will exit with non-zero exit code");
-            }
-        }
-
-        private async Task<int> Run()
-        {
-            var pid = Process.GetCurrentProcess().Id;
-            Log.Logger.Information($"Long running process started. PID={pid}");
-
-            var parentPid = _configRoot.GetValue<int?>("ParentProcessId");
-
-            using (parentPid.HasValue
-                ? new ProcessExitedHelper(parentPid.Value, _ => ParentExited(parentPid.Value), new NullLoggerFactory())
-                : NoopDisposable.Instance)
-            {
-                using (await CooperativeShutdown.Listen(ExitRequested, new NullLoggerFactory()))
-                {
-                    // Poll the shutdown token in a tight loop
-                    while(!_shutdown.IsCancellationRequested || _ignoreShutdownSignal)
-                    {
-                        await Task.Delay(100);
-                    }
-                    Log.Information("Exiting.");
-                }
-            }
-
-            return _exitWithNonZero ? -1 : 0;
-        }
-
-        static Task<int> Main(string[] args) => new Program(args).Run();
-
-        private void ExitRequested()
-        {
-            Log.Logger.Information("Cooperative shutdown requested.");
-
-            if (_ignoreShutdownSignal)
-            {
-                Log.Logger.Information("Shut down signal ignored.");
-                return;
-            }
-
-            _shutdown.Cancel();
-        }
-
-        private void ParentExited(int processId)
-        {
-            Log.Logger.Information($"Parent process {processId} exited.");
-            _shutdown.Cancel();
-        }
-
-        private class NoopDisposable : IDisposable
-        {
-            public void Dispose()
-            {}
-
-            internal static readonly IDisposable Instance = new NoopDisposable();
-        }
+        Log.Information("Exiting.");
     }
+}
+
+return exitWithNonZero ? -1 : 0;
+
+void ExitRequested()
+{
+    Log.Logger.Information("Cooperative shutdown requested.");
+
+    if (ignoreShutdownSignal)
+    {
+        Log.Logger.Information("Shut down signal ignored.");
+        return;
+    }
+
+    shutdown.Cancel();
+}
+
+void ParentExited(int processId)
+{
+    Log.Logger.Information($"Parent process {processId} exited.");
+    shutdown.Cancel();
+}
+
+class NoopDisposable : IDisposable
+{
+    public void Dispose()
+    { }
+
+    internal static readonly IDisposable Instance = new NoopDisposable();
 }
